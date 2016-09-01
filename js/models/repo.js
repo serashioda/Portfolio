46e50af7a73122d62f10326ff1fe6c0229d8edf2
serashioda <seras37@gmail.com>
2016-09-01 08:31:14 -0700

    refactored client request from $.ajax to $.when/ $.get

--- js/models/repo.js
+++ js/models/repo.js
@@ -2,21 +2,25 @@
   var reposObj = {};
 
   reposObj.allRepos = [];
-// TODO: DONE - create a githubToken.js file that we can use to generate our headers
-         // properly.
+  reposObj.followers = [];
+// create a githubToken.js file that we can use to generate our headers properly.
   //CALLBACK FUNCTION AFTER AJAX REQUEST FOR MY REPO DATA, CALLBACK IS CALLED WHEN LOAD IS COMPLETED
-  reposObj.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/serashioda/repos' +
-           '?per_page=5' +
-           '&sort=updated',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + token},
-      success: function(data) {
-        reposObj.allRepos = data;
-        callback();
-      }
-    });
+  $.when(
+    $.get('https://api.github.com/users/codefellows-seattle-301d10/repos' +
+          '?per_page=10' +
+          '&sort=updatd')
+          .done(function(data) {
+            reposObj.allRepos = data;
+          }),
+    $.get('/github.'+
+          '?per_page=10' +
+          '&sort=updatd')
+          .done(function(data) {
+            reposObj.allRepos = data;
+          })
+         // NOTE: since the 'data' paramter comes back as an
+         // array of objects, we can reassign allRepos below.
+    ).done(callback);
   };
 
   reposObj.withTheAttribute = function(myAttr) {

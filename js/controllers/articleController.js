99e40672a6473f30f73a441f28390f45f2c553a7
serashioda <seras37@gmail.com>
2016-09-01 13:18:11 -0700

    fixed minor sytax errors

--- js/controllers/articleController.js
+++ js/controllers/articleController.js
@@ -1,15 +1,16 @@
 (function(module) {
-  var articleController = {};
+  var articleController = {
 
     index: function() {
       $('.tab-content').fadeOut();
       // $('#filters').fadeIn();
       $('#articles').fadeIn();
+    }
+  };
 
     // Invoke the retrieval process for our data!
   //   Article.fetchAll(articleView.renderIndexPage);
   // };
 
   module.articleController = articleController;
-
 })(window);
--- js/models/repo.js
+++ js/models/repo.js
@@ -4,22 +4,23 @@
   reposObj.allRepos = [];
   reposObj.followers = [];
   //CALLBACK FUNCTION AFTER AJAX REQUEST FOR MY REPO DATA, CALLBACK IS CALLED WHEN LOAD IS COMPLETED
-  $.when(
-    $.get('/github/users/serashioda/repos' +
-          '?per_page=10' +
-          '&sort=updatd')
-          .done(function(data) {
-            reposObj.allRepos = data;
-          }),
-    $.get('/github/users/serashioda/followers'+
-          '?per_page=10' +
-          '&sort=updatd')
-          .done(function(data) {
-            reposObj.allRepos = data;
-          })
-         // NOTE: since the 'data' paramter comes back as an
-         // array of objects, we can reassign allRepos below.
-    ).done(callback);
+  reposObj.requestRepos = function(callback) {
+    $.when(
+      $.get('/github/users/serashioda/repos' +
+            '?per_page=10' +
+            '&sort=updatd')
+            .done(function(data) {
+              reposObj.allRepos = data;
+            }),
+      $.get('/github/users/serashioda/followers' +
+            '?per_page=10' +
+            '&sort=updatd')
+            .done(function(data) {
+              reposObj.allRepos = data;
+            })
+           // NOTE: since the 'data' paramter comes back as an
+           // array of objects, we can reassign allRepos below.
+      ).done(callback);
   };
 
   reposObj.withTheAttribute = function(myAttr) {
